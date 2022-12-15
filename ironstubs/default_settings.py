2990040385e6791180994c9ac310ff186100c97a
Jasper Delahaije <jdelahaije@gmail.com>
2022-12-15 15:29:19 +0100

    Update default_settings.py

--- ironstubs/default_settings.py
+++ ironstubs/default_settings.py
@@ -1,16 +1,15 @@
-import os
 
 PATHS = ["C:\\Program Files\\Siemens\\Automation\\Portal V15_1\\PublicAPI\\V15.1"]
 
 ASSEMBLIES = [
-    # 'System',
-    # 'System.Runtime.Serialization',
+    'System',
+    'System.Runtime.Serialization',
     "Siemens.Engineering",
-    # 'Siemens.Engineering.Hmi',
-    # "Siemens.Engineering.Hmi"
+    'Siemens.Engineering.Hmi',
+    'Siemens.Engineering.AddIn',
 ]
 
 BUILTINS = ["clr", "wpf"]
 
-# ASSEMBLIES.extend(BUILTINS)
+ASSEMBLIES.extend(BUILTINS)
 ASSEMBLIES.sort()
