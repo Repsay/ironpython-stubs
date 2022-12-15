adb2b5a486fcf54a8e515aab8f92b4dd6972b51c
Jasper Delahaije <jdelahaije@gmail.com>
2022-12-15 15:24:23 +0100

    Remove

--- release/stubs.min/Siemens/Engineering/HW/__init___parts/IHardwareCompareTarget.py
+++
@@ -1,5 +0,0 @@
-class IHardwareCompareTarget:
- """ Access to the device/device item in a HW compare scenario """
- def __init__(self,*args):
-  """ x.__init__(...) initializes x; see x.__class__.__doc__ for signaturex.__init__(...) initializes x; see x.__class__.__doc__ for signature """
-  pass
